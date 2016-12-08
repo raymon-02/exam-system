e1b7756a1796e99e81c1e78ad85851afd4bda4c7
Ilya Orlov <iorlov@GDSpb0719.local>
2016-12-09 01:11:13 +0300

    Create skeleton for project

---
+++ src/main/java/ru/university/examsystem/config/DaoConfig.java
@@ -0,0 +1,9 @@
+package ru.university.examsystem.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories
+public class DaoConfig {
+}
