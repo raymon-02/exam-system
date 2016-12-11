a443e6dfaaaf043d5d0febcfde96e7dac667b802
Ilya Orlov <2x2.4@mail.ru>
2016-12-11 20:22:25 +0300

    Rename home to getting started

--- src/main/java/ru/university/examsystem/controller/GettingStartedController.java
+++ src/main/java/ru/university/examsystem/controller/GettingStartedController.java
@@ -5,10 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class HomeController {
+public class GettingStartedController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String home() {
-        return "home";
+    public String gettingStarted() {
+        return "gettingstarted";
     }
 }
