2542debbcbfd90277e866f3655074a0b85048926
Ilya Orlov <2x2.4@mail.ru>
2016-12-11 17:29:42 +0300

    Add web security config

--- src/main/java/ru/university/examsystem/config/WebSecurityConfig.java
+++ src/main/java/ru/university/examsystem/config/WebSecurityConfig.java
@@ -1,9 +1,39 @@
 package ru.university.examsystem.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests()
+                    .antMatchers("/admin", "/admin/**").hasAuthority("ADMIN")
+                    .antMatchers("/prepod", "/prepod/**").hasAuthority("PROFESSOR")
+                    .antMatchers("/student", "/student/**").hasAuthority("STUDENT")
+                    .anyRequest().authenticated()
+                    .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                    .logout()
+                    .permitAll();
+    }
+
+    @Override
+    public UserDetailsService userDetailsServiceBean() {
+        return userDetailsService;
+    }
+
 }
