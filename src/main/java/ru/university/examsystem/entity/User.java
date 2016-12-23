40a23a1b56d45934864e1dc47363a6304ba1ee5a
Ilia Orlov <2x2.4@mail.ru>
2016-12-23 19:29:23 +0300

    Optimise import

--- src/main/java/ru/university/examsystem/entity/User.java
+++ src/main/java/ru/university/examsystem/entity/User.java
@@ -1,7 +1,6 @@
 package ru.university.examsystem.entity;
 
 import lombok.Data;
-import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
